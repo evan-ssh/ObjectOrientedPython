a9d6a5dd0c6498670023be4e7b67d9696830ed61
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-17 18:35:00 -0330

    Display transaction info method

--- bank.py
+++ bank.py
@@ -3,3 +3,5 @@ class Transaction:
         self.title = title
         self.amount = amount
         self.type = type
+    def display_info(self):
+        return f"|Transaction Details|\n |Expense{self.title}|\n |Amount: {self.amount}| \n |Expense Type{self.type}"
