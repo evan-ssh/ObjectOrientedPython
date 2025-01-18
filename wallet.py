d6779b61a0393be98637a2fb3c9b7ee249b6619f
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-18 18:15:55 -0330

    Input commands

--- wallet.py
+++ wallet.py
@@ -9,3 +9,20 @@ def main():
         print("3. Display All Transactions")
         print("4. Exit Program")
         print("======================================")
+
+        command = input("Enter command")
+        if command == "1":
+            wallet.AddTransactionMain()
+        elif command == "2":
+            name = input("Enter Name of Transaction to Delete:")
+            print(wallet.del_transaction(name))
+        elif command == "3":
+            print(wallet.display_transactions())
+        elif command == "4":
+            print("Exiting Finance Tracker")
+            break
+        else:
+            print("Invalid Choice")
+        
+if __name__ == "__main__":
+    main()
