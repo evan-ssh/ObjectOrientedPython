1b48b86e42558a0e053224c7f27a3b753bd9d197
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-18 18:10:52 -0330

    Display command menu

--- wallet.py
+++ wallet.py
@@ -2,3 +2,10 @@ from bank import Bank
 def main():
     wallet = Bank()
     wallet.load_file()
+    while True:
+        print("\n===== Personal Finance Tracker =====")
+        print("1. Add a Transaction")
+        print("2. Remove a Transaction")
+        print("3. Display All Transactions")
+        print("4. Exit Program")
+        print("======================================")
