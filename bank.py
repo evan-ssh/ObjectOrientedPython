593e7c608dc3e6dae3a401ae35a4686f5e506583
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-18 17:39:42 -0330

    Create an empty wallet to store transactions

--- bank.py
+++ bank.py
@@ -1,7 +1,13 @@
 class Transaction:
-    def __init__(self,title,amount,type):
-        self.title = title
+    def __init__(self,name,amount,type,note=""):
+        self.name = name
         self.amount = amount
         self.type = type
+        self.note = note
     def display_info(self):
         return f"|Transaction Details|\n |Expense{self.title}|\n |Amount: {self.amount}| \n |Expense Type{self.type}"
+    
+
+class Bank:
+    def __init__(self):
+        self.wallet = []
