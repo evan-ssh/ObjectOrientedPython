b81ce16eea79aaff57bc6665de8bc153e313aef1
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-19 17:50:04 -0330

    Construct Deck Class

--- BlacJack.py
+++ BlacJack.py
@@ -6,4 +6,10 @@ class Card:
         self.is_ace = is_ace
     def show_card(self):
         return(f"{self.face} of {self.suit}")
-        
+
+class Deck:
+    values = ["Ace","2","3","4","5","6","7","8","9","10","Jack","King","Queen"]
+    suits = ["Hearts", "Diamonds","Spades","Clubs"]
+
+    def __init__(self):
+        self.cards = []
