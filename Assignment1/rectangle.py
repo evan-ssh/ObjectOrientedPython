8f3720e3f0e2f7f3e81e455c613c565c17dcd4b7
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-22 19:38:14 -0330

    calculate perimeter

--- Assignment1/rectangle.py
+++ Assignment1/rectangle.py
@@ -2,4 +2,6 @@ class Rectangle:
     def __init__(self,height,width):
         self.height = height
         self.width = width
-        
+
+    def calculate_perimter(self):
+        return (self.height * 2) + (self.width * 2)
