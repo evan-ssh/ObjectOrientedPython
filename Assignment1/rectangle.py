3703f01a5b9f812f0fe9328bc6fb04108266755d
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-22 19:38:54 -0330

    method to calculate area

--- Assignment1/rectangle.py
+++ Assignment1/rectangle.py
@@ -5,3 +5,6 @@ class Rectangle:
 
     def calculate_perimter(self):
         return (self.height * 2) + (self.width * 2)
+    
+    def calculate_area(self):
+        return self.height * self.width 
