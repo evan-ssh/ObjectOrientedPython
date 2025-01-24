d596b2224f23e550c8b3035b15b9765a41da6cca
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-24 09:27:47 -0330

    method to show a card

--- Assignment1/cards.py
+++ Assignment1/cards.py
@@ -3,3 +3,7 @@ class Card:
     def __init__(self,rank,suit):
         self.rank = rank
         self.suit = suit
+    
+    def show_card(self):
+        return f"{self.rank} of {self.suit}"
+        
