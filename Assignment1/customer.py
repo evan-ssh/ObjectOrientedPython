59c0e5e5078667741cb5cc17ace5444c6697c10f
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-24 10:31:08 -0330

    method to read the customes csv file

--- Assignment1/customer.py
+++ Assignment1/customer.py
@@ -16,3 +16,13 @@ class Customer:
             return f"{self.cust_id}\n{self.first_name},{self.last_name}\n{self.company_name}\n{self.address}\n{self.city},{self.state},{self.zip}"
         else:
             return f"{self.cust_id}\n{self.first_name},{self.last_name}\n{self.address}\n{self.city},{self.state},{self.zip}"
+
+    def open_file(self):
+        customers = []
+        with open('customers.csv') as file:
+            csv_reader = csv.DictReader(file)
+            for row in csv_reader:
+                customer = Customer(cust_id = row['cust_id'].strip(),first_name = row['first_name'].strip(), last_name = row['last_name'].strip(),company_name = row['company_name'].strip(),address = row['address'].strip(), city = row['city'].strip(), state = row['state'].strip(), zip = row['zip'].strip())
+                customers.append(customer)
+        return customers
+        
