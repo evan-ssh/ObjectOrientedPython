4cc1b5d7dd313e895babfc84f10809c7852f05ea
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-24 09:54:29 -0330

    method to show info of a customer

--- Assignment1/customer.py
+++ Assignment1/customer.py
@@ -10,3 +10,9 @@ class Customer:
         self.city = city
         self.state = state
         self.zip = zip
+
+    def customer_info(self):
+        if self.company_name:
+            return f"{self.cust_id}\n{self.first_name},{self.last_name}\n{self.company_name}\n{self.address}\n{self.city},{self.state},{self.zip}"
+        else:
+            return f"{self.cust_id}\n{self.first_name},{self.last_name}\n{self.address}\n{self.city},{self.state},{self.zip}"
