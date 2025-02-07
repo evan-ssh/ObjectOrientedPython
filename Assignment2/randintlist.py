b4441d12aac8a375cb6525a20cfa47f86a1d4cb7
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-07 14:11:51 -0330

    Generate Numbers In Range 1-100 For the Randomlist Class When Constructing

--- Assignment2/randintlist.py
+++ Assignment2/randintlist.py
@@ -1,7 +1,13 @@
+import random
 class List:
     def __init__(self):
         self.integers = []
 
 class RandomIntList(List):
-    def __init__(self):
+    def __init__(self,number_of_ints):
         List.__init__(self)
+        self.addInt(number_of_ints)
+    def addInt(self,number_of_ints):
+        for i in range(number_of_ints):
+            self.integers.append(random.randint(1,100))
+    
