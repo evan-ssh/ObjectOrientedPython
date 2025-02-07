79a1d5b763b1371b3cadf55615481c499edc4c5c
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-07 14:22:40 -0330

    Display Person Method, Uses isInstance to Determine What Object To Display

--- Assignment2/company.py
+++ Assignment2/company.py
@@ -7,7 +7,11 @@ class Person():
     def FullName(self):
         return f"First Name: {self.firstName}\nLast Name: {self.lastName}\nEmail: {self.email}"
     
-
+    def displayPerson(self):
+        if isinstance(self,Customer):
+            print(f"CUSTOMER\nName:{self.firstName}\nEmail:{self.email}\nCustomer ID:{self.cust_id}")
+        elif isinstance(self,Employee):
+            print(f"Name:{self.firstName}\nEmail:{self.email}\nSSN:{self.ssn}")
 class Employee(Person):
     def __init__(self,firstName, lastName, email, ssn):
         Person.__init__(self,firstName,lastName,email)
