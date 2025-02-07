2c8f622a0421e00c279772c9c45b811f86207fb8
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-07 12:11:54 -0330

    Construct Bart Class That Inherits From Player

--- Assignment2/rps.py
+++ Assignment2/rps.py
@@ -2,3 +2,8 @@ class Player():
     def __init__(self,name,hand):
         self.name = name
         self.hand = hand
+
+class Bart(Player):
+    def  __init__(self):
+        Player.__init__(self,"Bart","")
+        
