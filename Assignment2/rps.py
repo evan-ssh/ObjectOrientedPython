2031d3493e7df89f9b152831efc37fdfe2fd2ec7
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-07 12:14:05 -0330

    Construct Lisa Class

--- Assignment2/rps.py
+++ Assignment2/rps.py
@@ -9,3 +9,8 @@ class Bart(Player):
     def  __init__(self):
         Player.__init__(self,"Bart","")
         self.generateRoshambo()
+
+class Lisa(Player):
+    def __init__(self):
+        Player.__init__(self,"Lisa","")
+        self.generateRoshambo()
