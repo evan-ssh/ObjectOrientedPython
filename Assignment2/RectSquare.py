ac69c970c5826c92ce219864aa8dcea0795db75b
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-06 23:17:38 -0330

    Rectangle Class

---
+++ Assignment2/RectSquare.py
@@ -0,0 +1,17 @@
+class Rectangle:
+    def __init__(self,height,width):
+        self.height = height
+        self.width = width
+
+    def calculate_perimeter(self):
+        return (self.height * 2) + (self.width * 2)
+    
+    def calculate_area(self):
+        return self.height * self.width 
+    
+    def display_rectangle(self):
+        for row in range(self.height):
+            if row == 0 or row == self.height - 1:
+                print('*' * self.width)
+            else:
+                print('*' + (' ' * (self.width-2)) + '*')
