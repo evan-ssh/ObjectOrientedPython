0b5ca93574eb909f660800c85c05a3a853195bbf
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-07 14:34:43 -0330

    Module Test

--- Assignment2/company.py
+++ Assignment2/company.py
@@ -4,7 +4,7 @@ class Person():
         Self.lastName = lastName
         Self.email = email
 
-    def FullName(self):
+    def __str__(self):
         return f"First Name: {self.firstName}\nLast Name: {self.lastName}\nEmail: {self.email}"
     
     def displayPerson(self):
@@ -17,13 +17,24 @@ class Employee(Person):
         Person.__init__(self,firstName,lastName,email)
         self.ssn = ssn
     
-    def FullName(self):
+    def __str__(self):
         return f"Name: {self.firstName}\nEmail: {self.email}\nSSN: {self.ssn}"
     
 class Customer(Person):
     def __init__(self, firstName, lastName, email,cust_id):
         Person.__init__(self,firstName,lastName,email)
         self.cust_id = cust_id
-    def FullName(self):
+    def __str__(self):
         return f"Name: {self.firstName}\n Email: {self.email}\n Customer ID: {self.cust_id}"
-    
+
+if __name__ == "__main__":
+    #__str__
+    person = Person("Joel","Murach","12334@murach.com")
+    print(person)
+    customer = Customer("Frank","Wilson","frank@gmail.com","23432434")
+    print(customer)
+    employee = Employee("John","Apple","Johnapple123.com","324-343-434")
+    print(employee)
+    ##
+    employee.displayPerson()
+    customer.displayPerson()
