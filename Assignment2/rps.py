8f91ee07997bc67b6164a2db691b81f0c1f8e94c
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-07 12:20:39 -0330

    Play Method To Determine Win Cases

--- Assignment2/rps.py
+++ Assignment2/rps.py
@@ -6,6 +6,15 @@ class Player():
 
     def generateRoshambo(self):
         self.hand = "rock"
+
+    def play(self,opponent):
+        if self.hand == opponent.hand:
+            return None
+        elif self.hand == "paper" and opponent.hand == "rock" or self.hand == "rock" and opponent.hand == "scissors" or self.hand == "scissors" and opponent.hand == "paper":
+            return self
+        else:
+            return opponent
+    
 class Bart(Player):
     def  __init__(self):
         Player.__init__(self,"Bart","")
@@ -19,3 +28,5 @@ class Lisa(Player):
     def generateRoshambo(self):
         ListOfHands = ["rock","paper","scissors"]
         self.hand = random.choice(ListOfHands)
+
+    
