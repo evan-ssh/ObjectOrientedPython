ab86429282107258bf93d80823b3b3858137cf04
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-07 14:14:11 -0330

    Convert List To String Seperating Values With Comma And Space

--- Assignment2/randintlist.py
+++ Assignment2/randintlist.py
@@ -7,7 +7,13 @@ class RandomIntList(List):
     def __init__(self,number_of_ints):
         List.__init__(self)
         self.addInt(number_of_ints)
+
     def addInt(self,number_of_ints):
         for i in range(number_of_ints):
             self.integers.append(random.randint(1,100))
     
+    def show(self):
+        return ','.join(map(str,self.integers))
+
+randlist = RandomIntList(5)
+print(randlist.show())
