b2d6ece0500534d48c052634eef51b9db624d510
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-13 21:15:49 -0330

    Test Code

--- feb13thex.py
+++ feb13thex.py
@@ -22,4 +22,17 @@ class Hospital:
         for doc in self.doctors:
             yield doc
         
+
+if __name__ == "__main__":        
+ d1 = Doctor("Eric","3232323")
+ d2 = Doctor("John","40343443")
+ s1 = Surgeon("Timmy","3434242423","Trauma")
+ s2 = Surgeon("Kenny","34343","Heart")
+ hospital = Hospital()
+ hospital.addDoc(d1)
+ hospital.addDoc(d2)
+ hospital.addDoc(s1)
+ hospital.addDoc(s2)
+ for doc in hospital:
+    print(doc)  
                  
