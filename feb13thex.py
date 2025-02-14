d739371709405006bc6b54c5b7792e36c9af0162
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-13 21:10:55 -0330

    Str Method For Surgeon

--- feb13thex.py
+++ feb13thex.py
@@ -9,3 +9,6 @@ class Doctor:
 class Surgeon(Doctor):
     def __init__(self,name,salary,specialty):
         Doctor.__init__(self,name,salary)
+        self.specialty = specialty
+    def __str__(self):
+        return f"Dr.{self.name} Salary: {self.salary} Specialty: {self.specialty}"
