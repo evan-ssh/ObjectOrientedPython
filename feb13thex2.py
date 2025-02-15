4d7cf92a443507d52afb68c0815038984d36749d
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-15 14:50:46 -0330

    Displaying Collection of Games

--- feb13thex2.py
+++ feb13thex2.py
@@ -4,7 +4,7 @@ class Games:
         self.platform = platform
         self.value = value
     def __str__(self):
-        return f"Game: {self.title} Platform: {self.platform} Value: {self.value}"
+        return f"{self.title} Platform: {self.platform} Value:{self.value}"
 class Collection:
     def __init__(self):
         self.games = []
@@ -13,3 +13,17 @@ class Collection:
     def __iter__(self):
         for game in self.games:
             yield game
+
+g1 = Games("The Incredible Hulk","Nintendo Gamecube", 80)
+g2 = Games("Enternal Darkness","Nintendo Gamecube",110)
+g3 = Games("Metal Gear Solid: The Twin Snakes","Nintendo Gamecube",100)
+g4 = Games("Unreal Tournament: Collectors Edition(NEW)", "Windows PC", 90)
+collection = Collection()
+collection.addGame(g1)
+collection.addGame(g2)
+collection.addGame(g3)
+collection.addGame(g4)
+
+print("Erics Epic Collection\n")
+for game in collection:
+    print(game)
