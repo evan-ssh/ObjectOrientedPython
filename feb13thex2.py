1c3e85a4de7881590e8b7d7b9ef0d530953a4c88
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-15 12:56:26 -0330

    Construct Collection Class

--- feb13thex2.py
+++ feb13thex2.py
@@ -5,4 +5,11 @@ class Games:
         self.value = value
     def __str__(self):
         return f"Game: {self.title} Platform: {self.platform} Value: {self.value}"
-        
+class Collection:
+    def __init__(self):
+        self.games = []
+    def addGame(self,game):
+        self.games.append(game)
+    def __iter__(self):
+        for game in self.games:
+            yield game
