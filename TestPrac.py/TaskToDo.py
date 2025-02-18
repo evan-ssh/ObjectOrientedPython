394951d84e9f5d71e43ba03a839997c2d89a3674
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-18 18:08:51 -0330

    Display Completed Tasks and Iter method to display all

--- TestPrac.py/TaskToDo.py
+++ TestPrac.py/TaskToDo.py
@@ -17,10 +17,17 @@ class ToDoList:
 
     def addTask(self,task):
         self.tasks[task.id] = task
-        
+
     def markTaskCompleted(self,task_id):
         if task_id in self.tasks:
             self.tasks[task_id].markDone()
+    def displayCompleted(self):
+        for task in self.tasks.values():
+            if task.completed:
+                print(task)
+    def __iter__(self):
+        for task in self.tasks:
+            yield task
 
          
     
