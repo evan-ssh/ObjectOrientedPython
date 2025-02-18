53e2ad7dddea2b6e40c4d22881787bbed0bc6a8a
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-18 17:44:10 -0330

    Create a Class For Tasks

---
+++ TestPrac.py/TaskToDo.py
@@ -0,0 +1,8 @@
+class Task:
+    def __init__(self,id,name,description,completed):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.completed = completed  
+    
+    
