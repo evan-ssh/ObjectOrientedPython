e4f945543669f7589a6b280d08431e98695185b3
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-18 18:04:47 -0330

    Add a Task

--- TestPrac.py/TaskToDo.py
+++ TestPrac.py/TaskToDo.py
@@ -3,6 +3,20 @@ class Task:
         self.id = id
         self.name = name
         self.description = description
-        self.completed = completed  
+        self.completed = False
+
+    def markDone(self):
+        self.completed = True
+
+    def __str__(self):
+        return f"{self.name} = {'Completed' if self.completed else 'Not completed'}"
     
+class ToDoList:
+    def __init__(self):
+        self.tasks = {}
+
+    def addTask(self,task):
+        self.tasks[task.id] = task
+
+         
     
