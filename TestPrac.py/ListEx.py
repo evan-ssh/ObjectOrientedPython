5cf4823813dd31a57ad8ac3cb60b527d85338882
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-20 12:25:38 -0330

    Remove From List

--- TestPrac.py/ListEx.py
+++ TestPrac.py/ListEx.py
@@ -4,4 +4,9 @@ class ListManger:
     
     def addToList(self,item):
         self.list.append(item)
-        
+
+    def RemoveFromList(self,item):
+        if item in self.list:
+            self.list.remove(item)
+        else:
+            print("Item not in list")
