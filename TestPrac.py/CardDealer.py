d17191f87d2015bec668ac360f382ada9e7ce676
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-21 19:26:54 -0330

    Generate Deck of cards

--- TestPrac.py/CardDealer.py
+++ TestPrac.py/CardDealer.py
@@ -1,10 +1,38 @@
 class Card:
+    rank = ['1','2','3','4','5','6','7','8','9','10','Jacks','Queen','King']
+    suite = ['Hearts','Diamonds','Clubs','Spades']
     def __init__(self,rank,suite):
-        self.rank = ['1','2','3','4','5','6','7','8','9','10','Jacks','Queen','King']
-        self.suite = ['Hearts','Diamonds','Clubs','Spades']
+        self.rank = rank
+        self.suite = suite
+       
     
 
     def __str__(self):
         return f"{self.rank} of {self.suite}"
         
 
+
+class Deck:
+    def __init__(self):
+        self.DeckOfCards = []
+    
+    def generateDeck(self):
+        for rank in Card.rank:
+            for suit in Card.suite:
+                self.DeckOfCards.append(Card(rank,suit))
+    
+    def amountCards(self):
+        return len(self.DeckOfCards)
+    
+    def dealCard(self):
+        self.DeckOfCards.pop()
+    
+    def __iter__(self):
+        for card in self.DeckOfCards:
+            yield card
+
+
+deck = Deck()
+deck.generateDeck()
+for card in deck:
+    print(card)
