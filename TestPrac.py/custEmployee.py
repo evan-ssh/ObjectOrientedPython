1c920b0227bb9117ec3850b83412afc63e290f91
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-23 19:54:11 -0330

    Inherit using super class

---
+++ TestPrac.py/custEmployee.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self,firstName,lastName,email):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+    def __str__(self):
+        return f"FirstName:{self.firstName} LastName:{self}."
+        
+class Customer(Person):
+    def __init__(self,firstName,lastName,email,cust_id):
+        super().__init__(firstName,lastName,email)
