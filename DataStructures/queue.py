f889520d2892cd5f41445ef60daf3e0a42ae84d7
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-24 20:07:29 -0330

    dequeue customer

--- DataStructures/queue.py
+++ DataStructures/queue.py
@@ -8,3 +8,9 @@ class Queue:
 
     def enqueue(self,customer):
         self.customerLine.append(customer)
+    
+    def dequeue(self):
+        if len(self.customerLine) != 0:
+            return self.customerLine.pop(0)
+        else:
+            return "Cannot remove from an empty line"
