71f146aac857b99205e146b1a567f8816d4b7d56
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-24 20:12:47 -0330

    Main Method

--- DataStructures/queue.py
+++ DataStructures/queue.py
@@ -14,3 +14,22 @@ class Queue:
             return self.customerLine.pop(0)
         else:
             return "Cannot remove from an empty line"
+
+def displayMenu():
+    print("1. Add customer to line")
+    print("2. Remove customer from line")
+    print("3. Exit Program")
+
+def main():
+    queue = Queue()
+    displayMenu()
+    while True:
+        command = int(input("Enter a command"))
+        if command == 1:
+            customer = input("Enter customer name")
+            queue.enqueue(customer)
+        elif command == 2:
+            customer = queue.dequeue()
+            print(f"{customer} was removed from the line")
+    
+main()
