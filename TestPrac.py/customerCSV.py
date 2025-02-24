4212cf4da6ef71eda9337ad550311557fe0ecd5f
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-24 19:40:36 -0330

    Method to open file

--- TestPrac.py/customerCSV.py
+++ TestPrac.py/customerCSV.py
@@ -1,6 +1,6 @@
 import csv
 class Customer:
-    def __init__(self,cust_id,first_name,last_name,company_name,address,city,state,zip)
+    def __init__(self,cust_id,first_name,last_name,company_name,address,city,state,zip):
         self.cust_id = cust_id
         self.first_name = first_name
         self.last_name = last_name
@@ -11,4 +11,24 @@ class Customer:
         self.zip = zip
 
     def __str__(self):
-        return f"Name:{self.cust_id}\nLastName:{self.last_name}\nCompany{self.company_name}
+        return f"Name:{self.cust_id}\nLastName:{self.last_name}\nCompany{self.company_name}"
+
+        
+def openFile():
+    filename = 'customers.csv'
+    customers = []
+    with open(filename) as file:
+        reader = csv.reader(file)
+        for row in reader:
+            cust_id = row[0]
+            first_name = row[1]
+            last_name = row[2]
+            company_name = row[3]
+            address = row[4]
+            city = row[5]
+            state = row[6]
+            zipcode = row[7]
+            customer = Customer(cust_id,first_name,last_name,company_name,address,city,state,zipcode)
+            customers.append(customer)
+    return customers
+
