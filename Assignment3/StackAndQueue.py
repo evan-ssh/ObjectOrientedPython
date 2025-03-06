bebffefb047538d7461f80959757581b54c04688
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-03-06 16:35:15 -0330

    peek method for queue

--- Assignment3/StackAndQueue.py
+++ Assignment3/StackAndQueue.py
@@ -26,19 +26,29 @@ class Stack:
     def __str__(self):
         return f"Stack Items: {len(self.stack)}"
         
-    class Queue:
-        def __init__(self):
-            self.queue = []
+class Queue:
+    def __init__(self):
+        self.queue = []
 
-        def enqueue(self, item):
-            self.queue.append(item)
-        def deque(self,item):
-            if len(self.queue) != 0:
-                return self.pop(0)
-            else:
-                return "Cannot remove from empty queue"
-        def __str__(self):
-            f"Queue {len(self.queue)}"
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def deque(self):
+        if len(self.queue) != 0:
+            return self.pop()
+        else:                
+            return "Cannot remove from empty queue"
+    
+    def peek(self):
+        if len(self.queue) != 0:
+            return self.pop()
+        else:                
+            return "Cannot remove from empty queue"
+
+    def length(self):
+        return len(self.queue)
+    def __str__(self):
+        f"Queue {len(self.queue)}"
 
 def displayMenu():
     print("1. Start a stack")
@@ -64,6 +74,22 @@ def main():
         print(f"{poppedItem} was popped from stack")
         peekItem = stack.peek()
         print(f"{peekItem} was peeked from stack")
+        print(stack)
+    elif command == 2:
+        queue = Queue()
+        i1 = "a"
+        i2 = "b"
+        i3 = "c"
+        queue.enqueue(i1)
+        print(f"{i1} was added to the queue")
+        queue.enqueue(i2)
+        print(f"{i2} was added to the queue")
+        queue.enqueue(i3)
+        print(f"{i3} was added to the queue")
+        print(f"There are {queue.length} items in queue")
+        dequeItem = queue.deque(i1)
+        print(f"{dequeItem} was returned from the front of the queue")
+        print
 
 if __name__ == "__main__":
     main()
