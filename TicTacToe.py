77f2718a661ea5c7e606e1112d97f7908f829bbd
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-03-06 16:03:19 -0330

    CheckRows for win

--- TicTacToe.py
+++ TicTacToe.py
@@ -13,3 +13,18 @@ class Board:
             return True
         else:
             return False
+
+    def checkRows(self):
+        # Horizontal lines
+        for row in self.board:
+            if row[0] == row[1] == row[2] != "_":
+                return True
+        # Vertical lines
+        for col in range(3):
+            if self.board[0][col] == self.board[1][col] == self.board[2][col] != "_":
+                return True
+        # Diagonal lines
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] != "_" or \
+           self.board[0][2] == self.board[1][1] == self.board[2][0] != "_":
+            return True
+        return False
