09af7a797544aef32d9f2a514d5a1cdfccdb5a02
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-03-05 21:18:23 -0330

    ShowBoard

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,8 @@
 class Board:
     def __init__(self):
         self.board = [["_","_","_"],["_","_","_"],["_","_","_"]]
-        
+
+    def showBoard(self):
+        print("_" * 7)
+        for row in self.board:
+            print("|" + "|".join(row) + "|")  
