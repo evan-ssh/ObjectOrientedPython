cabab56c1d4731e3e324700b7821524b52c43db6
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-03-07 18:11:50 -0330

    Create getter metod for each private attribute

--- Test1/EricStore.py
+++ Test1/EricStore.py
@@ -3,3 +3,17 @@ class Item:
         self.__name = name
         self.__quantity = quantity
         self.__price = price
+
+    @property
+    def name(self):
+        return self.__name
+    
+    @property
+    def quantity(self):
+        return self.__quantity
+    
+    @property
+    def price(self):
+        return self.__price
+    
+    
