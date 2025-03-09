ac91580e90dac4e7026e625d324dcb95216a1cd8
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-03-08 21:38:35 -0330

    Problems

---
+++ listpractice.py
@@ -0,0 +1,8 @@
+#Extract unique characters from multiple strings
+
+fruits = ["apple", "banana", "grape", "orange"]
+
+#Flatten list
+nested_list = [[1, 2, 3], [4, 5], [6, 7, 8], [9]]
+
+#Find all divisors of a number (n)
