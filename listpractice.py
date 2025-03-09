eb411c5161f24763c22aa679b58daac7445e22c0
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-03-08 21:42:38 -0330

    loop for unique characters

--- listpractice.py
+++ listpractice.py
@@ -1,6 +1,12 @@
 #Extract unique characters from multiple strings
-
 fruits = ["apple", "banana", "grape", "orange"]
+uniqueChars = []
+for fruit in fruits:
+    for char in fruit:
+        if char not in uniqueChars:
+            uniqueChars.append(char)
+print(uniqueChars)
+
 
 #Flatten list
 nested_list = [[1, 2, 3], [4, 5], [6, 7, 8], [9]]
