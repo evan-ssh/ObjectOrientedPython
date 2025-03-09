a4aec5da864a6181e237a6f494e07cfb5588db2d
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-03-08 21:50:58 -0330

    loop to flatten list

--- listpractice.py
+++ listpractice.py
@@ -1,14 +1,16 @@
 #Extract unique characters from multiple strings
-fruits = ["apple", "banana", "grape", "orange"]
-uniqueChars = []
-for fruit in fruits:
-    for char in fruit:
-        if char not in uniqueChars:
-            uniqueChars.append(char)
-print(uniqueChars)
-
+#fruits = ["apple", "banana", "grape", "orange"]
+#uniqueChars = []
+#for fruit in fruits:
+#    for char in fruit:
+#        if char not in uniqueChars:
+#            uniqueChars.append(char)
+#print(uniqueChars)
 
 #Flatten list
 nested_list = [[1, 2, 3], [4, 5], [6, 7, 8], [9]]
-
+flatten_list = []
+for i in nested_list:
+    flatten_list.extend(i)
+print(flatten_list)
 #Find all divisors of a number (n)
