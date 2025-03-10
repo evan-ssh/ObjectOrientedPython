3bac1611f8cbe2dbb515ee523b89a857ca586746
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-03-10 09:29:06 -0230

    Benchmark

--- DataStructures/LinearSearchEx.py
+++ DataStructures/LinearSearchEx.py
@@ -1,13 +1,21 @@
 #Write a function which performs linear search.  The function should accept 2 arguments, an array to search with and a value to search for.  The function should return True or False.  Test the function by creating a list with 1000 random values and choose a random value to search for.
+import random, time
 
+array = []
+for _ in range(1000000):
+    array.append(random.randint(1,100))
+    
+searchValue = random.randint(1,100)
 
-array = [1,23,4,1,3,4,6]
-def search(array,value):
-    for i in range(len(array)):
-        if i == value:
+def search(array,searchValue):
+    for i in array:
+        if i == searchValue:
             return True
-        
     return False
 
-test = search(array,3)
+start_time = time.time()
+test = search(array,searchValue)
 print(test)
+end_time = time.time()
+total_time = end_time - start_time
+print(total_time)
