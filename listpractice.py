162814717040635c305a90bd84f69086c68aceae
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-03-10 10:03:42 -0300

    Intersection of lists

--- listpractice.py
+++ listpractice.py
@@ -8,17 +8,22 @@
 #print(uniqueChars)
 
 #Flatten list
-nested_list = [[1, 2, 3], [4, 5], [6, 7, 8], [9]]
-flatten_list = []
-for i in nested_list:
-    flatten_list.extend(i)
-print(flatten_list)
+#nested_list = [[1, 2, 3], [4, 5], [6, 7, 8], [9]]
+#flatten_list = []
+#for i in nested_list:
+#    flatten_list.extend(i)
+#print(flatten_list)
 #Find all divisors of a number (n)
 
-n = 5
-factors = []
-for i in range(1, n+1):
-    if n % i == 0:
-        factors.append(i)
-print(factors)
-        
+#n = 5
+#factors = []
+#for i in range(1, n+1):
+#    if n % i == 0:
+#        factors.append(i)
+#print(factors)
+
+#Elements in common between two lists
+fruits = ["apple","bannana","kiwi"]
+fruits2 = ["grape","watermelon","kiwi"]
+intersectFruits = [fruit for fruit in fruits if fruit in fruits2]
+print(intersectFruits)
