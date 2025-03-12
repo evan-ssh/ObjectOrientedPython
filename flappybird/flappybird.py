d61c574e64e4d415133611f49514d4458f8eb303
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-03-12 18:54:54 -0230

    Create list to store bird images

---
+++ flappybird/flappybird.py
@@ -0,0 +1,8 @@
+import pygame
+import neat
+import time
+import os
+WIN_WIDTH = 600
+WIN_HEIGHT = 800
+
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs_b286d95d6d","bird1.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs_b286d95d6d","bird2.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs_b286d95d6d","bird3.png")))]
