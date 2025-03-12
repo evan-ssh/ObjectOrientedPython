cbb2c821d060ed3b298eb4c0977e1c26116fcf09
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-03-12 18:56:56 -0230

    BG BASE AND PIPE IMG LISt

--- flappybird/flappybird.py
+++ flappybird/flappybird.py
@@ -6,3 +6,6 @@ WIN_WIDTH = 600
 WIN_HEIGHT = 800
 
 BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs_b286d95d6d","bird1.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs_b286d95d6d","bird2.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs_b286d95d6d","bird3.png")))]
+PIPE_IMG = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs_b286d95d6d","pipe.png")))]
+BASE_IMG = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs_b286d95d6d","base.png")))]
+BG_IMG = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs_b286d95d6d","bg.png")))]
