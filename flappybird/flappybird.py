5546137424acfdbfd2f408a76644856b68962e0f
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-03-13 09:21:55 -0230

    Move method for bird

--- flappybird/flappybird.py
+++ flappybird/flappybird.py
@@ -30,4 +30,21 @@ class Bird:
         self.tick_count = 0
         self.height = self.y
     
+    def move(self):
+        self.tick_count += 1
 
+        d = self.vel * self.tick_count + 1.15 * self.tick_count ** 2
+        if d >= 16:
+            d = 16
+        if d < 0:
+            d -= 2
+
+        self.y = self.y + d
+
+        if d < 0 or self.y < self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+                self.title = self.MAX_ROTATION
+        else:
+            if self.tilt > -90:
+                self.tily -= self.ROT_VEL
+while True:
