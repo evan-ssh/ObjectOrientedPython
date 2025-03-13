910d33c3412fee2b3b34b17a100eec53c352ee6d
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-03-13 11:32:10 -0230

    Move method for base to look like scrolling

--- flappybird/flappybird.py
+++ flappybird/flappybird.py
@@ -122,6 +122,15 @@ class Base:
         self.x1 = 0
         self.x2 = self.WIDTH
 
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+        
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
 def draw_window(win,bird):
     win.blit(BG_IMG, (0,0))
     bird.draw()
